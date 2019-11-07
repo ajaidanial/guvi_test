4c8d41ee97f992a8105888ea77809871a93c86f3
ajaidanial <ajaidanial@gmail.com>
2019-11-08 04:43:54 +0530

    topbar | 2

--- src/layouts/components/Topbar/Topbar.js
+++ src/layouts/components/Topbar/Topbar.js
@@ -8,7 +8,8 @@ import {
   Hidden,
   IconButton,
   Avatar,
-  Divider
+  Divider,
+  Typography
 } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import { ExpandMore, Notifications } from '@material-ui/icons'
@@ -24,11 +25,24 @@ const useStyles = makeStyles((theme) => ({
     width: 45,
     height: 45
   },
+  award1: {
+    width: 'auto',
+    height: 30
+  },
   dividerFull: {
-    // width: 45,
     height: 48
   },
-  topbarWidgetSpacing: {}
+  dividerHalf: {
+    height: 22
+  },
+  topbarWidgetSpacing: {},
+  text: {
+    color: 'white'
+  },
+  levelImg: {
+    width: 'auto',
+    height: 30
+  }
 }))
 
 const Topbar = (props) => {
@@ -40,6 +54,65 @@ const Topbar = (props) => {
     <AppBar {...rest} className={clsx(classes.root, className)}>
       <Toolbar>
         <div className={classes.flexGrow} />
+        {/* section 1 start */}
+        <img
+          alt="Global Logo"
+          className={clsx(classes.levelImg, classes.topbarWidgetSpacing)}
+          src="images/thunder.png"
+        />
+        <Typography
+          className={clsx(classes.text, classes.topbarWidgetSpacing)}
+          component="h5"
+          variant="h5"
+        >
+          1234434
+        </Typography>
+        {/* section 1 end */}
+        {/* section 2 start */}
+        <Divider className={classes.dividerHalf} orientation="vertical" />
+        <img
+          alt="Global Logo"
+          className={clsx(classes.levelImg, classes.topbarWidgetSpacing)}
+          src="images/global.png"
+        />
+        <Typography
+          className={clsx(classes.text, classes.topbarWidgetSpacing)}
+          component="h5"
+          variant="h5"
+        >
+          1234434
+        </Typography>
+        {/* section 2 end */}
+        {/* section 3 start */}
+        <Divider className={classes.dividerHalf} orientation="vertical" />
+        <Typography
+          className={clsx(classes.text, classes.topbarWidgetSpacing)}
+          component="h5"
+          variant="h5"
+        >
+          Rank : 1234434
+        </Typography>
+        {/* section 3 end */}
+        {/* section 4 start */}
+        <Divider className={classes.dividerHalf} orientation="vertical" />
+        <Typography
+          className={clsx(classes.text, classes.topbarWidgetSpacing)}
+          component="h5"
+          variant="h5"
+        >
+          Level
+        </Typography>
+        <img
+          alt="Level"
+          className={clsx(classes.levelImg, classes.topbarWidgetSpacing)}
+          src="images/number1.png"
+        />
+        <img
+          alt="Topbar Award"
+          className={clsx(classes.award1, classes.topbarWidgetSpacing)}
+          src="images/award1.png"
+        />
+        {/* section 4 end */}
         {/* section 5 start */}
         <Divider className={classes.dividerFull} orientation="vertical" />
         <IconButton

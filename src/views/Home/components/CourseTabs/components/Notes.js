61b4cd01457c49110bf3bd1af9620fba54bae07d
ajaidanial <ajaidanial@gmail.com>
2019-11-09 22:26:33 +0530

    tabs | done | 1

--- src/views/Home/components/CourseTabs/components/Notes.js
+++ src/views/Home/components/CourseTabs/components/Notes.js
@@ -1,5 +1,38 @@
 import React from 'react'
+import { TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  textField: {
+    width: '100%'
+  },
+  button: {},
+  btnContainer: {
+    width: '100%',
+    padding: 0,
+    marginTop: theme.spacing(3),
+    textAlign: 'right'
+  }
+}))
 
 export default function Notes() {
-  return <div>Notes</div>
+  const classes = useStyles()
+
+  return (
+    <>
+      <TextField
+        className={classes.textField}
+        label="Add your notes..."
+        multiline
+        rows="15"
+        variant="outlined"
+      />
+
+      <div className={classes.btnContainer}>
+        <Button className={classes.button} color="primary" variant="contained">
+          Save
+        </Button>
+      </div>
+    </>
+  )
 }
--- src/views/Home/components/CourseTabs/components/Summary.js
+++ src/views/Home/components/CourseTabs/components/Summary.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
+const useStyles = makeStyles((theme) => ({
+  headings: {
+    marginBottom: theme.spacing(2)
+  },
+  contents: {
+    lineHeight: 2
+  }
+}))
 export default function Summary() {
-  return <div>Summary</div>
+  const classes = useStyles()
+
+  return (
+    <>
+      <Typography className={classes.headings} variant="h5">
+        Key Takeaways From This Chapter
+      </Typography>
+      <Typography className={classes.contents}>1. Test line 1</Typography>
+      <Typography className={classes.contents}>2. Test line 2</Typography>
+      <Typography className={classes.contents}>3. Test line 3</Typography>
+    </>
+  )
 }

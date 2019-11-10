196dedad4cb3de7deca7b068fb25b000efcfce41
ajaidanial <ajaidanial@gmail.com>
2019-11-10 18:14:03 +0530

    button added

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,10 +1,12 @@
 import { createMuiTheme } from '@material-ui/core'
 import palette from './palette'
 import typography from './typography'
+import overrides from './overrides'
 
 const theme = createMuiTheme({
   palette,
-  typography
+  typography,
+  overrides
 })
 export default theme
 
---
+++ src/theme/overrides/MuiButton.js
@@ -0,0 +1,7 @@
+export default {
+  contained: {
+    paddingLeft: '45px',
+    paddingRight: '45px',
+    borderRadius: '20px'
+  }
+}

ca253767aafc82dbb62033213b3e728b6c1445ae
ajaidanial <ajaidanial@gmail.com>
2019-11-10 11:03:55 +0530

    accordian start

--- src/views/Home/components/TableOfContent/TableOfContent.js
+++ src/views/Home/components/TableOfContent/TableOfContent.js
@@ -1,7 +1,59 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { styles } from './styles'
 
-export default class TableOfContent extends Component {
-  render() {
-    return <div>TableOfContent</div>
+export default function TableOFContent() {
+  const classes = makeStyles(styles)()
+  // for panel options
+  const [expanded, setExpanded] = React.useState(false)
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false)
   }
+
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>General settings</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+            Aliquam eget maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        expanded={expanded === 'panel2'}
+        onChange={handleChange('panel2')}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography className={classes.heading}>Users</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat
+            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
+            laoreet laoreet.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  )
 }
---
+++ src/views/Home/components/TableOfContent/styles.js
@@ -0,0 +1,6 @@
+export const styles = (theme) => ({
+  root: {
+    width: '100%'
+  },
+  heading: {}
+})
